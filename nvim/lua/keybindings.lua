0cc1e617718957a2d119e189ec707b46fc06d7bf
saiumr <colaloveanan@gmail.com>
2023-01-09 16:27:37 +0800

    fixed keybindings

--- nvim/lua/keybindings.lua
+++ nvim/lua/keybindings.lua
@@ -70,9 +70,9 @@ local pluginKeys = {}
 pluginKeys.cmp = function(cmp)
   return {
     -- 上一个
-    ['<tab>'] = cmp.mapping.select_prev_item(),
+    ['<S-tab>'] = cmp.mapping.select_prev_item(),
     -- 下一个
-    ['<S-tab>'] = cmp.mapping.select_next_item(),
+    ['<tab>'] = cmp.mapping.select_next_item(),
     -- 出现补全
     ['<A-.>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     -- 取消
