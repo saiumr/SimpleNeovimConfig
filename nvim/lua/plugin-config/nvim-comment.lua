b844b073f91c499d80bb4f519abbab1cf79f623f
saiumr <colaloveanan@gmail.com>
2023-02-17 13:48:26 +0800

    (fix): Change *.c *.cpp *.h *.hpp commentstring to '// %s'
    Signed-off-by: saiumr <colaloveanan@gmail.com>

--- nvim/lua/plugin-config/nvim-comment.lua
+++ nvim/lua/plugin-config/nvim-comment.lua
@@ -3,7 +3,7 @@ require('nvim_comment').setup(
     -- Linters prefer comment and line to have a space in between markers
     marker_padding = true,
     -- should comment out empty or whitespace only lines
-    comment_empty = true,
+    comment_empty = false,
     -- trim empty comment whitespace
     comment_empty_trim_whitespace = false,
     -- Should key mappings be created
@@ -15,6 +15,11 @@ require('nvim_comment').setup(
     -- text object mapping, comment chunk,,
     comment_chunk_text_object = "ic",
     -- Hook function to call before commenting takes place
-    hook = nil
+    hook = function()
+      local filetype = vim.api.nvim_buf_get_option(0, 'filetype')
+      if filetype == 'c' or filetype == 'cpp' or filetype == 'h' or filetype == 'hpp' then
+        vim.api.nvim_buf_set_option(0, 'commentstring', '// %s')
+      end
+    end
   }
 )
